5e4d99b0814d1777b8e423156141230ee05b9299
Ozod26 <ozodfarid46@gmail.com>
2024-09-29 05:04:23 +0900

    sorting ages

---
+++ index.js
@@ -0,0 +1,7 @@
+const ages = [12, 24, 55, 90, 75, 31, 10, 7, 4];
+console.log(ages);
+
+console.log("============================");
+
+const newAges = ages.sort((a, b) => a - b);
+console.log(newAges);
